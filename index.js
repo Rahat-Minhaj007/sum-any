0aba1441eca6141e9654e524d176b99fc58b7ea1
F Shrabon <fshrabon151@gmail.com>
2022-03-31 13:15:57 +0600

    index documentaion added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const sumArray =(obj)=>obj.reduce((prev,current)=>prev+current,0)
-const sumArrayObject =(obj,key)=>obj.reduce((prev,current)=>prev+current[key],0)
-const sumArrayObjectIndex =(obj,index)=>obj.reduce((prev,current)=>prev+Object.values(current)[index],0)
-module.exports = {sumArray,sumArrayObject,sumArrayObjectIndex};
+const sumArray = (array = []) =>
+  array.reduce((prev, current) => prev + current, 0);
+const sumArrayObject = (array = [], key = "") =>
+  array.reduce((prev, current) => prev + current[key], 0);
+const sumArrayObjectIndex = (array = [], index = 0) =>
+  array.reduce((prev, current) => prev + Object.values(current)[index], 0);
+module.exports = { sumArray, sumArrayObject, sumArrayObjectIndex };
